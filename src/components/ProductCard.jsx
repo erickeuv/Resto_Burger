e291971b6a9ab3600779b0efb2f989d114878efb
Erick Urdaneta <133135692+erickeuv@users.noreply.github.com>
2024-10-05 16:49:46 -0300

    fix

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,11 @@ function ProductCard({ title, imageUrl, description }) {
   return (
     <div className="flex flex-col bg-white shadow-sm border border-slate-200 rounded-lg my-6 w-96"> {/* Contenedor principal */}
       <div className="m-2.5 overflow-hidden rounded-md h-80 flex justify-center items-center"> {/* Contenedor de la imagen */}
-        <img className="w-full h-full object-cover" src={imageUrl} alt={title || 'Producto'} /> {/* Imagen del producto */}
+        <img 
+          className="w-full h-full object-cover" 
+          src={imageUrl} 
+          alt={title || 'Producto'} 
+        /> {/* Imagen del producto */}
       </div>
       <div className="p-6 text-center"> {/* Contenedor del texto */}
         <h4 className="mb-1 text-xl font-semibold text-slate-800">{title || 'Título del Producto'}</h4> {/* Título */}
