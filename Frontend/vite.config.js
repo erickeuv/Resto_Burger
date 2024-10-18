cf008ff5ac9d7340dbd093b1f426768e04fef2fa
Erick Urdaneta <133135692+erickeuv@users.noreply.github.com>
2024-10-18 19:55:02 -0300

    .

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  }
+}
