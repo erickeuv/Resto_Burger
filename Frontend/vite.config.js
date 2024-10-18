4662d026f5982d162393ebf6a0ad444cb97201ff
Erick Urdaneta <133135692+erickeuv@users.noreply.github.com>
2024-10-18 20:12:47 -0300

    .

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default {
-  server: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
-  }
-}
+  base: './',  
+};
